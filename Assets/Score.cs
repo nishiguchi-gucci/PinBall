d44a802f3bafc579c00ebaea51dff0456c2f63e8
nishiguchi-gucci <ah3yr6gucci@gmail.com>
2020-07-09 15:50:35 +0900

    Lesson5課題途中です。

---
+++ Assets/Score.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+
+    GameObject scoreText;//テキスト
+
+    int score = 0;//スコア
+
+    
+
+    // Use this for initialization
+    void Start () {
+        this.scoreText = GameObject.Find("ScoreText");//オブジェクト
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (tag == "SmallStarTag")
+        {
+            score += 10;
+
+        }
+        else if (tag == "LargeStarTag")
+        {
+            score += 20;
+
+        }
+        else if (tag == "SmallCloudTag")
+        {
+            score += 50;
+
+        }
+        else if (tag == "LargeCloudTag")
+        {
+            score += 100;
+        }
+        else
+        {
+            score += 0;
+        }
+
+        this.scoreText.GetComponent<Text>().text = "" + score;//テキスト
+    }
+}
